23779a13342800b9bb01f8d9e0b430a3883bdb3d
byui-professional <men22027@byui.edu>
2026-06-05 21:57:59 -0600

    Create FitTrack Pro landing page

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("FitTrack Pro Loaded");
+document.getElementById("goalBtn").addEventListener("click", () => {
+
+  const weight = prompt("Enter your weight in pounds:");
+
+  if(!weight) return;
+
+  const calories = Math.round(weight * 15);
+
+  alert(
+    `Estimated maintenance calories: ${calories} calories/day`
+  );
+
+});
